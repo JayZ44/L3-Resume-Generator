053fa63eb520e427ad08b2d7ffa3ef36fa4593ac
JayZ44 <jazonyounge@pursuit.org>
2025-03-12 15:01:08 -0400

    feat: users now recieve a cover letter along with their resume and feedback. Slightly improved css.

--- L3-Resume-Generator/FRONTEND/src/Form.jsx
+++ L3-Resume-Generator/FRONTEND/src/Form.jsx
@@ -21,6 +21,7 @@ const Form = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [resume, setResume] = useState("");
   const [resumeFeedback, setResumeFeedback] = useState("");
+  const [coverLetter, setCoverLetter] = useState("");
 
   const handleSubmit = async () => {
     if (!prompt.Name.trim()) {
@@ -32,7 +33,6 @@ const Form = () => {
     try {
       const response = await fetch(
         `http://localhost:3003/api/generate-from-transcript`,
-        // `${import.meta.env.VITE_BASE_API_URL}/api/generate-from-transcript`,
         {
           method: "POST",
           headers: {
@@ -51,12 +51,11 @@ const Form = () => {
 
       setResume(dataArr[0]);
       setResumeFeedback(dataArr[1]);
+      setCoverLetter(dataArr[2]);
       console.log("Resume generated:");
       console.log("Feedback generated:", data.feedback);
-      // Handle successful response here
     } catch (error) {
       console.error("Error generating resume:", error);
-      // Handle error here
     } finally {
       setIsLoading(false);
     }
@@ -67,116 +66,165 @@ const Form = () => {
       <div className="form-container">
         <form onSubmit={(e) => e.preventDefault()}>
           <div className="input-fields">
-            <input
-              type="text"
-              placeholder="Desired Position..."
-              value={prompt.DesiredPosition}
-              onChange={(e) =>
-                setPrompt({ ...prompt, DesiredPosition: e.target.value })
-              }
-              required
-            />
-            <input
-              type="text"
-              placeholder="Full Name..."
-              value={prompt.Name}
-              onChange={(e) => setPrompt({ ...prompt, Name: e.target.value })}
-              required
-            />
-            <input
-              type="number"
-              placeholder="Phone Number..."
-              value={prompt.PhoneNumber}
-              onChange={(e) =>
-                setPrompt({ ...prompt, PhoneNumber: e.target.value })
-              }
-              required
-            />
-            <input
-              type="email"
-              placeholder="Email..."
-              value={prompt.Email}
-              onChange={(e) => setPrompt({ ...prompt, Email: e.target.value })}
-              required
-            />
-            <input
-              type="text"
-              placeholder="LinkedIn..."
-              value={prompt.LinkedIn}
-              onChange={(e) =>
-                setPrompt({ ...prompt, LinkedIn: e.target.value })
-              }
-              required
-            />
-            <input
-              type="text"
-              placeholder="City and state..."
-              value={prompt.CityState}
-              onChange={(e) =>
-                setPrompt({ ...prompt, CityState: e.target.value })
-              }
-              required
-            />
-            <input
-              type="text"
-              placeholder="Address..."
-              value={prompt.Address}
-              onChange={(e) =>
-                setPrompt({ ...prompt, Address: e.target.value })
-              }
-              required
-            />
-            <input
-              type="text"
-              placeholder="Zip Code..."
-              value={prompt.ZipCode}
-              onChange={(e) =>
-                setPrompt({ ...prompt, ZipCode: e.target.value })
-              }
-              required
-            />
-            <input
-              type="text"
-              placeholder="Education..."
-              value={prompt.Education}
-              onChange={(e) =>
-                setPrompt({ ...prompt, Education: e.target.value })
-              }
-              required
-            />
-            <input
-              type="text"
-              placeholder="Work Experience..."
-              value={prompt.WorkExperience}
-              onChange={(e) =>
-                setPrompt({ ...prompt, WorkExperience: e.target.value })
-              }
-              required
-            />
-            <textarea
-              placeholder="Enter your skills, separated by commas..."
-              value={prompt.Skills}
-              onChange={(e) => setPrompt({ ...prompt, Skills: e.target.value })}
-              required
-            />
-            <input
-              type="text"
-              placeholder="Certifications..."
-              value={prompt.Certifications}
-              onChange={(e) =>
-                setPrompt({ ...prompt, Certifications: e.target.value })
-              }
-              required
-            />
-            <input
-              type="text"
-              placeholder="Summary..."
-              value={prompt.Summary}
-              onChange={(e) =>
-                setPrompt({ ...prompt, Summary: e.target.value })
-              }
-              required
-            />
+            <div className="left-col">
+              <label>
+                Desired Position:
+                <input
+                  type="text"
+                  placeholder="Desired Position..."
+                  value={prompt.DesiredPosition}
+                  onChange={(e) =>
+                    setPrompt({ ...prompt, DesiredPosition: e.target.value })
+                  }
+                  required
+                />
+              </label>
+              <label>
+                Full Name:
+                <input
+                  type="text"
+                  placeholder="Full Name..."
+                  value={prompt.Name}
+                  onChange={(e) =>
+                    setPrompt({ ...prompt, Name: e.target.value })
+                  }
+                  required
+                />
+              </label>
+              <label>
+                Phone Number:
+                <input
+                  type="text"
+                  placeholder="Phone Number..."
+                  value={prompt.PhoneNumber}
+                  onChange={(e) =>
+                    setPrompt({ ...prompt, PhoneNumber: e.target.value })
+                  }
+                  required
+                />
+              </label>
+              <label>
+                Email:
+                <input
+                  type="email"
+                  placeholder="Email..."
+                  value={prompt.Email}
+                  onChange={(e) =>
+                    setPrompt({ ...prompt, Email: e.target.value })
+                  }
+                  required
+                />
+              </label>
+              <label>
+                LinkedIn:
+                <input
+                  type="text"
+                  placeholder="LinkedIn..."
+                  value={prompt.LinkedIn}
+                  onChange={(e) =>
+                    setPrompt({ ...prompt, LinkedIn: e.target.value })
+                  }
+                  required
+                />
+              </label>
+              <label>
+                City and State:
+                <input
+                  type="text"
+                  placeholder="City and state..."
+                  value={prompt.CityState}
+                  onChange={(e) =>
+                    setPrompt({ ...prompt, CityState: e.target.value })
+                  }
+                  required
+                />
+              </label>
+            </div>
+            <div className="right-col">
+              <label>
+                Address:
+                <input
+                  type="text"
+                  placeholder="Address..."
+                  value={prompt.Address}
+                  onChange={(e) =>
+                    setPrompt({ ...prompt, Address: e.target.value })
+                  }
+                  required
+                />
+              </label>
+              <label>
+                Zip Code:
+                <input
+                  type="text"
+                  placeholder="Zip Code..."
+                  value={prompt.ZipCode}
+                  onChange={(e) =>
+                    setPrompt({ ...prompt, ZipCode: e.target.value })
+                  }
+                  required
+                />
+              </label>
+              <label>
+                Education:
+                <input
+                  type="text"
+                  placeholder="Education..."
+                  value={prompt.Education}
+                  onChange={(e) =>
+                    setPrompt({ ...prompt, Education: e.target.value })
+                  }
+                  required
+                />
+              </label>
+              <label>
+                Work Experience:
+                <input
+                  type="text"
+                  placeholder="Work Experience..."
+                  value={prompt.WorkExperience}
+                  onChange={(e) =>
+                    setPrompt({ ...prompt, WorkExperience: e.target.value })
+                  }
+                  required
+                />
+              </label>
+              <label>
+                Skills:
+                <input
+                  placeholder="Enter your skills, separated by commas..."
+                  value={prompt.Skills}
+                  onChange={(e) =>
+                    setPrompt({ ...prompt, Skills: e.target.value })
+                  }
+                  required
+                />
+              </label>
+              <label>
+                Certifications:
+                <input
+                  type="text"
+                  placeholder="Certifications..."
+                  value={prompt.Certifications}
+                  onChange={(e) =>
+                    setPrompt({ ...prompt, Certifications: e.target.value })
+                  }
+                  required
+                />
+              </label>
+              <label>
+                Summary:
+                <input
+                  type="text"
+                  placeholder="Summary..."
+                  value={prompt.Summary}
+                  onChange={(e) =>
+                    setPrompt({ ...prompt, Summary: e.target.value })
+                  }
+                  required
+                />
+              </label>
+            </div>
           </div>
         </form>
 
@@ -185,33 +233,27 @@ const Form = () => {
         </button>
       </div>
       <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <div style={{ flex: 1, marginRight: "10px" }}>
+        <div style={{ flex: 1, marginRight: "1px" }}>
           <h2>Resume:</h2>
           <div className="resumeBox">
             <ReactMarkdown>{resume}</ReactMarkdown>
           </div>
         </div>
         <div style={{ flex: 1, marginLeft: "10px" }}>
-          <h2>Feedback:</h2>
+          <h2>Letter:</h2>
           <div className="resumeBox">
             <ReactMarkdown>{resumeFeedback}</ReactMarkdown>
+          </div>
+        </div>
+        <div style={{ flex: 1, marginLeft: "10px" }}>
+          <h2>Feedback:</h2>
+          <div className="resumeBox">
+            <ReactMarkdown>{coverLetter}</ReactMarkdown>
           </div>
         </div>
       </div>
     </div>
   );
-  /*
-
-Skills
-:
-"tech,programming,computer repair, fullstack development, ruby on rails, python, javascript, react, AWS"
-Certifications
-:
-"High School Diploma"
-Summary
-:
-"Ive been in tech school forever. I'm so tired and i just want to get a job already"
-*/
 };
 
 export default Form;
